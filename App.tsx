7e4b3f38fd9f6273c7d584b03692c165b2ba3c50
matinabajracharya13 <22252274@students.latrobe.edu.au>
2026-05-24 22:05:07 +1000

    fix: import issue

--- App.tsx
+++ App.tsx
@@ -3,8 +3,7 @@ import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { initDatabase } from './src/services/sqliteService';
-import './src/tasks/backgroundSyncTask';
-import { registerBackgroundSyncTask } from './src/tasks/backgroundSyncTask';
+import { registerBackgroundSyncTask } from './tasks/backgroundSyncTask';
 
 export default function App() {
   useEffect(() => {
