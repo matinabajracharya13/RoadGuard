35c55ae30cc43767f5d495716f0be357eb040fcd
NabinSLa <22189222@students.latrobe.edu.au>
2026-05-24 10:44:08 +0000

    sync reports in background

--- App.tsx
+++ App.tsx
@@ -3,10 +3,13 @@ import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { initDatabase } from './src/services/sqliteService';
+import './src/tasks/backgroundSyncTask';
+import { registerBackgroundSyncTask } from './src/tasks/backgroundSyncTask';
 
 export default function App() {
   useEffect(() => {
     initDatabase();
+    registerBackgroundSyncTask();
   }, []);
 
   return (
