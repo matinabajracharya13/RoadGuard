ed45d245d0266e4da4df9831d067e469a816a4f2
nabinsla <22189222@students.latrobe.edu.au>
2026-05-24 21:21:43 +1000

    Unit and Integration test

---
+++ tasks/backgroundSyncTask.ts
@@ -0,0 +1,30 @@
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundTask from 'expo-background-task';
+import { syncPendingReports } from '../services/syncService';
+
+export const BACKGROUND_SYNC_TASK = 'ROADGUARD_BACKGROUND_SYNC';
+
+TaskManager.defineTask(BACKGROUND_SYNC_TASK, async () => {
+  try {
+    await syncPendingReports();
+    console.log('Background sync task running...');
+
+    return BackgroundTask.BackgroundTaskResult.Success;
+  } catch (error) {
+    console.log('Background sync failed:', error);
+
+    return BackgroundTask.BackgroundTaskResult.Failed;
+  }
+});
+
+export const registerBackgroundSyncTask = async () => {
+  const isRegistered = await TaskManager.isTaskRegisteredAsync(
+    BACKGROUND_SYNC_TASK
+  );
+
+  if (!isRegistered) {
+    await BackgroundTask.registerTaskAsync(BACKGROUND_SYNC_TASK, {
+      minimumInterval: 15,
+    });
+  }
+};
